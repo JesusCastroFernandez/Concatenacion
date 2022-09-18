b35c2fe56a3a6e9788e3c92e322c93d69358f051
JesusCastroFernandez <castrofernandezjesus@gmail.com>
2022-09-18 08:40:23 +0200

    Concatenacion
    Concatenar

---
+++ src/concatenación/Concatenación.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package concatenación;
+
+/**
+ *
+ * @author JESUS
+ */
+public class Concatenación {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String cadena="La";
+        
+        cadena+=" casa de";
+        System.out.println(cadena);
+        
+        cadena+=" Juan es";        
+        System.out.println(cadena);
+                
+        cadena+=" el número ";        
+        System.out.println(cadena);
+        
+        cadena+=25 + ".";        
+        System.out.println(cadena);        
+    }
+}
